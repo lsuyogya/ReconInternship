8fc5f993057d50be26c3f79777e794b0cb1faa77
Suyogya Luitel <lsuyogya@gmail.com>
2023-01-25 00:21:18 +0545

    chore - navUI loginForm

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <RouterView></RouterView>
+	<RouterView></RouterView>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /* color: #ffffff; */
-  /* background-color: #2c3e50; */
-  background-color: #c2d9f170;
-  color: #2c3e50;
-  margin: 0;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	/* color: #ffffff; */
+	/* background-color: #2c3e50; */
+	/* background-color: #c2d9f170; */
+	color: #2c3e50;
+	margin: 0;
+	min-height: 100vh;
+	display: grid;
+	place-content: center;
 }
 body {
-  margin: 0;
+	margin: 0;
 }
 </style>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,75 +1,98 @@
-<script>
-import { reactive, ref } from "vue";
-import { RouterLink, useRouter } from "vue-router";
-import hashPw from "../features/SHAhash";
+<script setup>
+import { reactive, ref } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+import hashPw from '../features/SHAhash';
+import {
+	NForm,
+	NFormItem,
+	NInput,
+	NButton,
+	NDivider,
+	NText,
+	NModal,
+	NCard,
+} from 'naive-ui';
 
-export default {
-  setup() {
-    const loginState = reactive({ username: "", password: "" });
-    const errors = ref([]);
-    const router = useRouter();
+const loginState = reactive({ username: '', password: '' });
+const errors = ref([]);
+const router = useRouter();
+const hasError = ref(false);
 
-    async function handleLogin() {
-      verifyForm(loginState);
+async function handleLogin() {
+	hasError.value = false;
+	verifyForm(loginState);
 
-      if (errors.value.length) return;
-      localStorage.removeItem("currentUser");
-      const user = localStorage.getItem(loginState.username);
+	console.log(hasError.value);
+	if (hasError.value) return;
+	localStorage.removeItem('currentUser');
+	const user = localStorage.getItem(loginState.username);
+	if (!user) {
+		hasError.value = true;
+		errors.value.push(`Provided user does not exist. Try signing up`);
+		return;
+	}
 
-      if (!user) {
-        //counld make it an error msg as well instead of alert
-        alert("Provided user does not exist. Try signing up");
-        return;
-      }
+	const userDetails = JSON.parse(user);
+	const hashedPw = await hashPw(loginState.password);
+	if (hashedPw === userDetails.password) {
+		localStorage.setItem(
+			'currentUser',
+			JSON.stringify({ username: loginState.username, password: hashedPw })
+		);
+		router.push({ name: 'dashboard' });
+	}
+}
 
-      const userDetails = JSON.parse(user);
-      const hashedPw = await hashPw(loginState.password);
-      if (hashedPw === userDetails.password) {
-        localStorage.setItem(
-          "currentUser",
-          JSON.stringify({ username: loginState.username, password: hashedPw })
-        );
-        router.push({ name: "dashboard" });
-      }
-    }
-
-    function verifyForm(formState) {
-      errors.value = [];
-      for (const [key, value] of Object.entries(formState)) {
-        if (value === "") {
-          errors.value.push(`${key} cannot be left empty`);
-        }
-      }
-    }
-
-    return { loginState, handleLogin, errors };
-  },
-};
+function verifyForm(formState) {
+	errors.value = [];
+	for (const [key, value] of Object.entries(formState)) {
+		if (value === '') {
+			hasError.value = true;
+			errors.value.push(`${key} cannot be left empty`);
+		}
+	}
+}
 </script>
 
 <template>
-  <span v-if="errors.length">
-    <strong>The following errors occured:</strong>
-    <ul>
-      <li v-for="error in errors">{{ error }}</li>
-    </ul>
-  </span>
-  <form @submit.prevent="handleLogin">
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username" v-model="loginState.username" />
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" v-model="loginState.password" />
-    <button type="submit">Login</button>
-    <p>
-      Don't have an account?
-      <RouterLink :to="{ name: 'register' }">Sign up</RouterLink>
-    </p>
-  </form>
+	<n-modal v-model:show="hasError">
+		<n-card style="width: 300px" title="The following errors occured">
+			<ul>
+				<li v-for="error in errors">{{ error }}</li>
+			</ul>
+		</n-card>
+	</n-modal>
+
+	<n-form @submit.prevent="handleLogin">
+		<n-form-item label="Username">
+			<n-input
+				type="text"
+				name="username"
+				id="username"
+				v-model:value="loginState.username"
+			/>
+		</n-form-item>
+		<n-form-item label="Password">
+			<n-input
+				type="password"
+				name="password"
+				id="password"
+				v-model:value="loginState.password"
+			/>
+		</n-form-item>
+
+		<n-button type="primary" attr-type="submit">Login</n-button>
+		<n-divider></n-divider>
+		<n-text type="primary">
+			Don't have an account?
+			<RouterLink :to="{ name: 'register' }">Sign up</RouterLink>
+		</n-text>
+	</n-form>
 </template>
 
 <style scoped>
 form {
-  display: grid;
-  place-content: center;
+	display: grid;
+	place-content: center;
 }
 </style>
